6b8725196fe2e79421b2178294e961bb83659929
guarantixnftdev <guarantixnft@gmail.com>
2025-05-16 21:22:38 +0300

    wrong directory

--- server/src/redis/dedup.client.ts
+++
@@ -1,9 +0,0 @@
-import { Redis } from "@upstash/redis";
-import dotenv from "dotenv"
-
-dotenv.config();
-
-export const dedupRedis = new Redis({
-  url: process.env.UPSTASH_REDIS_DEDUP_URL!,
-  token: process.env.UPSTASH_REDIS_DEDUP_TOKEN!,
-});
